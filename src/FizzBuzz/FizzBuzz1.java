487e21dd191122ed63e178ca1cdebe43bac9035b
kazunekochan13 <kazuneko78d@gmail.com>
2015-04-28 09:21:08 +0800

    added another algorithm

--- src/FizzBuzz/FizzBuzz1.java
+++ src/FizzBuzz/FizzBuzz1.java
@@ -4,11 +4,11 @@
  * and open the template in the editor.
  */
 
-package First;
+package FizzBuzz;
 
 public class FizzBuzz1 {
     
-    public static void main(String[] args){
+    public void fb1(){
         for (int i=1; i<101; i++){
             if (i%3 == 0){
                 if (i%5 ==0){
@@ -25,5 +25,26 @@ public class FizzBuzz1 {
                 System.out.println(i);
             }
         }
+    }
+    
+    public void fb2() {
+        for (int i=1; i<101; i++){
+            String n = "";
+            if (i%3==0){
+                n += "Fizz";
+            }
+            if (i%4==0){
+                n += "Buzz";
+            }
+            if (n==""){
+                n = String.valueOf(i);
+            }
+            System.out.println(n);
+        }
+    }
+    
+    public static void main(String[] args){
+        FizzBuzz1 main = new FizzBuzz1();
+        main.fb2();
     }
 }
